937e4db6a7cdfc51c1360c15c137b5aa3ab22fbf
Sasha Rudnev <ralex_06@mail.ru>
2021-10-23 16:47:56 +0300

    lab_2 part2

--- app/src/main/java/com/github/ralex2105/android_lab/MainActivity.kt
+++ app/src/main/java/com/github/ralex2105/android_lab/MainActivity.kt
@@ -1,39 +1,56 @@
 package com.github.ralex2105.android_lab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var secondsElapsed: Int = 0
+    lateinit var textSecondsElapsed: TextView
+
+    companion object {
+        const val SEC = "sec"
+    }
+
+    var backgroundThread = Thread {
+        while (true) {
+            Thread.sleep(1000)
+            textSecondsElapsed.post {
+                textSecondsElapsed.text = "Seconds elapsed: " + secondsElapsed++
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Activity_lifecycle", "Crate activity")
+        textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
+        backgroundThread.start()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("Activity_lifecycle", "Start activity")
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(SEC, secondsElapsed)
+        super.onSaveInstanceState(outState)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        secondsElapsed = savedInstanceState.getInt(SEC)
+        super.onRestoreInstanceState(savedInstanceState)
     }
 
+    /*
     override fun onResume() {
+        val shared = this.getPreferences(Context.MODE_PRIVATE)
+        secondsElapsed = shared.getInt(SEC, 0)
         super.onResume()
-        Log.d("Activity_lifecycle", "Resume activity")
     }
 
     override fun onPause() {
+        val shared = this.getPreferences(Context.MODE_PRIVATE)
+        with(shared.edit()) {
+            putInt(SEC, secondsElapsed)
+            apply()
+        }
         super.onPause()
-        Log.d("Activity_lifecycle", "Paused activity")
     }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("Activity_lifecycle", "Stop activity")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("Activity_lifecycle", "Destroy activity")
-    }
+     */
 }
